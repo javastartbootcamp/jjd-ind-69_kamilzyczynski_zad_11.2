7241f2345b8b34a476598ff2b68f1ded806530fe
kamilzyczynski <kamil.zyczynski@gmail.com>
2023-04-19 22:11:53 +0200

    Rozwiązanie

--- src/main/java/pl/javastart/task/Main.java
+++ src/main/java/pl/javastart/task/Main.java
@@ -4,5 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        try {
+            Person person = new Person("Jan", "Kowalski", 25, "1234567890");
+            System.out.println(person);
+            person.setAge(-10);
+            System.out.println(person);
+        } catch (RuntimeException ex) {
+            System.err.println(ex.getMessage());
+        }
     }
 }
